aedc68264d1e2068a8d7e676efbf9d840caad728
张威 <908377161@qq.com>
2017-01-25 00:47:32 +0800

    displaydatasetting

---
+++ src/pages/signIn/displaydatasetting/Displaydatasetting.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: "displaydatasetting.html"
+})
+export class Displaydatasetting implements OnInit {
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  public ngOnInit() {
+
+  }
+
+}
