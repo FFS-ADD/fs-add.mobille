f7bacf29576d00abf76c7250456882d044088fe4
baofeng.wu <baofeng.wu@accenture.com>
2017-02-13 17:38:33 +0800

    [modify] refs #barometer
    carl

--- src/pages/home/barometer/barometer.component.ts
+++ src/pages/home/barometer/barometer.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BarometerDetailPage } from './barometer-detail/barometer-detail'
+import { LastUpdateStatusInfo } from './barometer.lastProInfo'
 
 @Component({
     selector: 'barometer-area',
-    providers: [],
+    providers: [LastUpdateStatusInfo],
     templateUrl: 'barometer.html',
 })
 export class BarometerComponent {
 
-    constructor(public nav: NavController) {
-        // this.action.init();
+    constructor(public nav: NavController,
+                private lastStatus: LastUpdateStatusInfo) {
+      this.lastStatus.faceIcon = 'md-happy';
+      this.lastStatus.infoText = 'this is a test log';
+      this.lastStatus.statusColor = '2';
+      this.lastStatus.updateData = '2017/12/17';
     }
 
     detail() {
---
+++ src/pages/home/barometer/barometer.lastProInfo.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class LastUpdateStatusInfo {
+  faceIcon: string = '';
+  updateData: string = '';
+  infoText: string = '';
+  statusColor: string = '';
+}
