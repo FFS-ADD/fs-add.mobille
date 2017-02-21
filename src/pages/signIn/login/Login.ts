7439160684746dca4a6f52c620e9278a8074ca47
baofeng.wu <baofeng.wu@accenture.com>
2017-02-21 09:13:39 +0800

    [fix] refs #login  for dashing

--- src/pages/signIn/login/Login.ts
+++ src/pages/signIn/login/Login.ts
@@ -1,20 +1,20 @@
-import {Component, OnInit }from "@angular/core"; 
-import {NavController, ModalController }from 'ionic-angular'; 
-import LoginAction from "./login.action"; 
-import {LoginState }from "./login.state"; 
-import {LoginStore }from "./login.store"; 
-import {LoginRequestInterface }from './login.interface'; 
-import {HomePage }from '../../home/home'; 
-import {DataSetting }from '../datasetting/datasetting.component'; 
-import {UserService }from "../../../core/UserService"; 
-import {ModalDialogComponent }from "./modalDialog.component"; 
+import {Component, OnInit }from "@angular/core";
+import {NavController, ModalController }from 'ionic-angular';
+import LoginAction from "./login.action";
+import {LoginState }from "./login.state";
+import {LoginStore }from "./login.store";
+import {LoginRequestInterface }from './login.interface';
+import {HomePage }from '../../home/home';
+import {DataSetting }from '../datasetting/datasetting.component';
+import {UserService }from "../../../core/UserService";
+import {ModalDialogComponent }from "./modalDialog.component";
 
 @Component( {
-  providers:[LoginAction, LoginState, LoginStore], 
+  providers:[LoginAction, LoginState, LoginStore],
   templateUrl:"login.html"
 })
 export class Login implements OnInit {
-  private form:LoginRequestInterface; 
+  private form:LoginRequestInterface;
   constructor(public modalCtrl: ModalController, public navCtrl:NavController, private action:LoginAction, private state:LoginState, private store:LoginStore, private userService:UserService) {
 
   }
@@ -24,17 +24,17 @@ export class Login implements OnInit {
   }
 
   public login() {
-    console.debug("login before"); 
-    let observable = this.action.login(this.form); 
+    console.debug("login before");
+    let observable = this.action.login(this.form);
     observable.subscribe((data) =>  {
-      // if (this.userService.getLastLoginTime() == null) {
-      //   this.navCtrl.push(DataSetting, {"perPage" : "login"});
-      // } else {
-      //   this.navCtrl.push(HomePage);
-      // }
-      let modal = this.modalCtrl.create(ModalDialogComponent, { userId: 8675309 });
-      modal.present();
-    }); 
+      if (this.userService.getLastLoginTime() == null) {
+        this.navCtrl.push(DataSetting, {"perPage" : "login"});
+      } else {
+        this.navCtrl.push(HomePage);
+      }
+      // let modal = this.modalCtrl.create(ModalDialogComponent, { userId: 8675309 });
+      // modal.present();
+    });
 
   }
 }
